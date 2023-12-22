9685ab764ce7a41cf0190f094f00247280296e00
Ryan Token <ryantoken13@gmail.com>
2023-12-22 10:06:36 -0700

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Ryan Token on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
